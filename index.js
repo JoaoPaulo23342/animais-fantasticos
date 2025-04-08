613c457df59558d0db8986c14bc48b11e9ed3740
João <Joaopauloresendecosta55@gmail.com>
2025-04-08 07:49:30 -0300

    Refactor code by removing unused DOM selections and adding a new ForEach example for better clarity

--- index.js
+++ index.js
@@ -7,27 +7,14 @@
     const naoExiste = document.getElementById('nao-existe');
     output(naoExiste); // null
 */
-const animais = document.getElementById('animais');
+/* ForEach
+    Constantemente vamos selecionar uma lista de elementos do dom.
+    A melhor forma para interagirmos com os mesmos é ultilizando
+    o metodo forEach.
+    const imgs = document.querySelectorAll('img');
+    imgs.forEach((item) => {
+        console.log(item);
+    });
 
-const primeiraUl = document.querySelector('ul');
+*/
 
-const linksInternos = document.querySelectorAll('[href^="#"]');
-
-console.log(linksInternos); // retorna todos os links internos
-
-const gridSection = document.getElementsByClassName('grid-section');
-const gridSection2 = document.querySelectorAll('.grid-section');
-
-primeiraUl.classList.add('grid-section'); // adiciona a classe grid-section na primeira ul
-console.log(gridSection); // retorna uma coleção HTML
-console.log(gridSection2); // retorna uma NodeList 
-
-const arrayGrid = Array.from(gridSection); // transforma a coleção HTML em um array
-
-const img = document.querySelectorAll('.animais-lista img');
-console.log(img)
-
-const imagem = document.querySelectorAll('[src^="img/imagem"]'); 
-console.log(imagem); 
-
-const links = document.querySelectorAll('[href^="#"]'); // seleciona todos os links do site
