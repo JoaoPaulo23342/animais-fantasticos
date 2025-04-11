2f6efb68ef95d647b5f8f417613159b0b5f61128
João <Joaopauloresendecosta55@gmail.com>
2025-04-11 18:55:01 -0300

    Refactor image event handling and add scroll event listener for logging

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-const img = document.querySelector('img')
-function callback() {
-    console.log('joão')
-} 
-img.addEventListener('click', callback)
+
+
+
+const h1 = document.querySelector('h1')
+function handleEv(ev) {
+    console.log(ev.type, ev)
+}
+window.addEventListener('scroll', handleEv)
+
+const imgs = document.querySelectorAll('img')
+
+imgs.forEach((img) => {
+    console.log(img)
+})
