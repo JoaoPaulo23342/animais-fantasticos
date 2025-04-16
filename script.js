a357fb312c64ba9a1f885e4fedefb0a86c276180
João <Joaopauloresendecosta55@gmail.com>
2025-04-16 09:52:31 -0300

    Add js-menu class to navigation and enhance accordion styles for better interaction

--- script.js
+++ script.js
@@ -1,50 +1,65 @@
+
 function initTabNav() {
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
-
-if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('ativo')
-
-
-
-
-
-function activeTab (i) {
-    tabContent.forEach( (content) => {
-        content.classList.remove('ativo')
-    })
-    tabContent[i].classList.add('ativo')
-}
-
-tabMenu.forEach((item, index) => {
-    item.addEventListener('click', () => {
-        activeTab(index)
-    })
-})
-
-} 
-}
-
-initTabNav()
-
-function faq () {
-const ativo = 'ativo'
-function initAccordion() {
-    this.classList.add(ativo)
-    this.nextElementSibling.classList.toggle(ativo);
-}
-const accordionList = document.querySelectorAll('.js-accordion dt')
-accordionList[0].classList.add(ativo)
-accordionList[0].nextElementSibling.classList.add(ativo)
-accordionList.forEach((item) => {
-    item.addEventListener('click', initAccordion)
-})
-}
-faq()
-
-
-
-
-
-
-
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
+  
+    if(tabMenu.length && tabContent.length) {
+      tabContent[0].classList.add('ativo');
+  
+      function activeTab(index) {
+        tabContent.forEach((content) => {
+          content.classList.remove('ativo');
+        });
+        tabContent[index].classList.add('ativo');
+      }
+  
+      tabMenu.forEach((item, index) => {
+        item.addEventListener('click', () => {
+          activeTab(index);
+        });
+      });
+    }
+  }
+  initTabNav();
+  
+  
+  function initAccordion() {
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    const activeClass = 'ativo';
+    
+    if(accordionList.length) {
+      accordionList[0].classList.add(activeClass);
+      accordionList[0].nextElementSibling.classList.add(activeClass);
+  
+      function activeAccordion() {
+        this.classList.toggle(activeClass);
+        this.nextElementSibling.classList.toggle(activeClass);
+      }
+  
+      accordionList.forEach((item) => {
+        item.addEventListener('click', activeAccordion);
+      });
+    }
+  }
+  initAccordion();
+  
+  
+  function initScrollSuave() {
+    const linksInternos = document.querySelectorAll('.menu a[href^="#"]');
+  
+    function scrollToSection(event) {
+      event.preventDefault();
+      const href = event.currentTarget.getAttribute('href');
+      const section = document.querySelector(href);
+      
+      section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+  
+    linksInternos.forEach((link) => {
+      link.addEventListener('click', scrollToSection);
+    });
+  }
+  initScrollSuave();
